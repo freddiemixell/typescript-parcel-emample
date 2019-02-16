9b3df268788a98985a82a10a68fb82492204c904
freddiemixell <fmixell@gmail.com>
2019-02-16 17:35:55 -0500

    Added typed examples

--- index.ts
+++ index.ts
@@ -1,1 +1,8 @@
-console.log("Hello world")
+const isOpen: boolean = false
+const firstName: string = 'Freddie'
+const myAge: number = 28
+const list: [number, string] = [1,'Name']
+
+enum Job{ WebDev, ProjectManager, SoftwareEngineer }
+
+const job: Job = Job.WebDev
