bd0bc1051e30d005755521cf9fa61e28b19042d1
freddiemixell <fmixell@gmail.com>
2019-02-16 18:20:30 -0500

    adding typed function

--- index.ts
+++ index.ts
@@ -3,6 +3,20 @@ const firstName: string = 'Freddie'
 const myAge: number = 28
 const list: [number, string] = [1,'Name']
 
-enum Job{ WebDev, ProjectManager, SoftwareEngineer }
+enum Job{
+    WebDev,
+    ProjectManager,
+    SoftwareEngineer
+}
 
 const job: Job = Job.WebDev
+
+// Declaring Typed Functions
+
+const sayHello = (name = "Bruce Wayne", ...otherStuff: string[]): string => {
+    // Name still type checked since we're implicitly setting as a string
+    console.log(otherStuff)
+    return `Hello, ${name}`
+}
+
+sayHello("Freddie", "Engineer", "WebDev" )
