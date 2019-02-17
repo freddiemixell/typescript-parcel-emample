63d8bde13594b6340433e1c45f603ea3b9cfd1ff
freddiemixell <fmixell@gmail.com>
2019-02-16 19:39:14 -0500

    added person interface

--- index.ts
+++ index.ts
@@ -20,3 +20,28 @@ const sayHello = (name = "Bruce Wayne", ...otherStuff: string[]): string => {
 }
 
 sayHello("Freddie", "Engineer", "WebDev" )
+
+// Interfaces
+
+interface Person {
+    name: string, // Named Typed Params
+    age?: number
+}
+
+const printStats = ({name, age}: Person): string => { // Destructuring name and age out of Person
+    if (age) {
+        return `Name: ${name} Age: ${age}`    
+    }
+    return `Name: ${name}`
+}
+
+console.log(printStats({
+    name: 'Freddie',
+    age: 28
+}))
+
+// Reverse order: Our interface allows us to enter these in any order
+console.log(printStats({age: 28, name: "Freddie"}))
+
+// Age is an optional param as denoted by the ? after its name
+console.log(printStats({name: "Freddie"}))
